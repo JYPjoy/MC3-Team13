b19afa566279c21bcef427345dd823fbcbca16b0
mininim <minsoplee2015@gmail.com>
2023-07-17 15:39:32 +0900

    [ADD] 프로젝트 파일 추가

---
+++ MalBal/MalBal/MalBalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalBalApp.swift
+//  MalBal
+//
+//  Created by Eric Lee on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct MalBalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
