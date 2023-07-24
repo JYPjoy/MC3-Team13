4049caba22e9fb14df8876d6d946b0e64255a387
JOY <jypjoy@gmail.com>
2023-07-24 16:43:50 +0900

    feat: 공용 팝업뷰 추가

--- MalBal/MalBal/Resources/Components/GLPopupView.swift
+++ MalBal/MalBal/Resources/Components/GLPopupView.swift
@@ -8,13 +8,76 @@
 import SwiftUI
 
 struct GLPopupView: View {
+    
+    let iconName: String
+    let popupTitle: String
+    let contentsText: String
+    let confirmText: String
+    
+    let didTapCancel: () -> Void
+    let didTapConfirm: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct GLPopupView_Previews: PreviewProvider {
-    static var previews: some View {
-        GLPopupView()
+        ZStack {
+            Color(hex: "000000").opacity(0.5).ignoresSafeArea()
+            VStack {
+                Spacer()
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 35)
+                        .fill(Color.main2)
+                        .frame(width: 371, height: 322)
+                    VStack {
+                        Image(iconName)
+                            .frame(width: 50, height: 50)
+                            .padding()
+                        Text(popupTitle)
+                            .font(FontManager.shared.appleSDGothicNeo(.extrabold, 20))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                        Spacer()
+                            .frame(height:15)
+                        Text(contentsText)
+                            .font(FontManager.shared.appleSDGothicNeo(.semibold, 14))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.main4)
+                        Spacer()
+                            .frame(height:28)
+                        
+                        HStack {
+                            Button{
+                                didTapCancel()
+                            }label: {
+                                Text("취소")
+                                    .font(FontManager.shared.appleSDGothicNeo(.semibold, 16))
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .padding(.horizontal, 40)
+                                    .background(
+                                        Color(hex:"B5B5B5").cornerRadius(10)
+                                            .shadow(radius: 10)
+                                    )
+                            }
+                            Spacer()
+                                .frame(width: 15)
+                            
+                            Button{
+                                didTapConfirm()
+                            }label: {
+                                Text(confirmText)
+                                    .font(FontManager.shared.appleSDGothicNeo(.semibold, 16))
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .padding(.horizontal, 35)
+                                    .background(
+                                        Color(UIColor(Color.main5)).cornerRadius(10)
+                                            .shadow(radius: 10)
+                                    )
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
