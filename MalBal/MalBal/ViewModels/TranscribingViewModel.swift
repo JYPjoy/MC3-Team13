682bb4bf39b3a1c193200ffb83a3f66510c54c09
bumee <favvuri@naver.com>
2023-07-25 15:18:27 +0900

    feat: 1분씩 STT 끊는 거 대충 해봄 ㅎㅎ

--- MalBal/MalBal/ViewModels/TranscribingViewModel.swift
+++ MalBal/MalBal/ViewModels/TranscribingViewModel.swift
@@ -10,6 +10,8 @@ import Speech
 
 class TranscribingViewModel: ObservableObject {
     @Published var recognizedText = ""
+    @Published var divisionedText = ""
+    
     //한국어로 STT 구현한 Function (MC2 2중대6소대 Team STT 구현 함수 참고)
     func performKoreanSpeechToText(url: URL, completion: @escaping (Bool) -> Void) {
         guard let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ko_KR")) else {
@@ -40,12 +42,14 @@ class TranscribingViewModel: ObservableObject {
                         // MARK: 음성을 차례대로 정확하게 변환하기 위해
                         if result == nil {
                             self.recognizedText = "대화없음"
+                            self.divisionedText += " 여긴 번역이 안됨 "
                             completion(true)
                         } else if (result?.isFinal)! {
                             //가장 bestTranscription을 뽑아서 설정한다.
                             if let res = result?.bestTranscription.formattedString {
                                 self.recognizedText = res
-                                print(res)
+                                self.divisionedText += res
+                                //print(res)
                                 completion(true)
                             }
                         }
@@ -53,6 +57,73 @@ class TranscribingViewModel: ObservableObject {
             } else {
                 print("Error: Speech-API not authorized!");
             }
+        }
+    }
+
+    func splitM4AFileIntoOneMinuteSegments(inputFileURL: URL) {
+        let asset = AVAsset(url: inputFileURL)
+        let duration = CMTimeGetSeconds(asset.duration)
+        let oneMinute = CMTime(seconds: 60, preferredTimescale: 1)
+        var startTime = CMTime.zero
+
+        for index in 0...Int(duration / 60) {
+            let segmentTimeRange = CMTimeRange(start: startTime, duration: oneMinute)
+            let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetAppleM4A)
+
+            let segmentOutputURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("segment_\(index).m4a")
+
+            exportSession?.outputURL = segmentOutputURL
+            exportSession?.outputFileType = .m4a
+            exportSession?.timeRange = segmentTimeRange
+
+            exportSession?.exportAsynchronously(completionHandler: {
+                if let error = exportSession?.error {
+                    print("Error exporting segment \(index): \(error.localizedDescription)")
+                } else {
+                    print("Segment \(index) exported successfully: \(segmentOutputURL)")
+                }
+            })
+
+            startTime = CMTimeAdd(startTime, oneMinute)
+        }
+    }
+
+    
+    func removeFile(at url: URL) {
+        do {
+            try FileManager.default.removeItem(at: url)
+            print("Temporary file removed: \(url.lastPathComponent)")
+        } catch {
+            print("Error removing temporary file: \(error.localizedDescription)")
+        }
+    }
+    
+    func performSTTM4AFiles() {
+        do {
+            let tempDirURL = URL(fileURLWithPath: NSTemporaryDirectory())
+            let tempM4AFiles = try FileManager.default.contentsOfDirectory(at: tempDirURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles).filter { $0.pathExtension == "m4a" }
+
+            for fileURL in tempM4AFiles {
+                performKoreanSpeechToText(url: fileURL) {_ in
+                    print("해치웠나?")
+                }
+            }
+        } catch {
+            print("Error some files: \(error.localizedDescription)")
+        }
+    }
+    
+    func removeAllTempM4AFiles() {
+        do {
+            let tempDirURL = URL(fileURLWithPath: NSTemporaryDirectory())
+            let tempM4AFiles = try FileManager.default.contentsOfDirectory(at: tempDirURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles).filter { $0.pathExtension == "m4a" }
+
+            for fileURL in tempM4AFiles {
+                try FileManager.default.removeItem(at: fileURL)
+                print("Removed temporary file: \(fileURL.lastPathComponent)")
+            }
+        } catch {
+            print("Error removing temporary files: \(error.localizedDescription)")
         }
     }
 }
