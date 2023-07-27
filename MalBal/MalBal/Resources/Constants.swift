7a30699ba60a0c7381ab59bc2df05106df0d9600
JOY <jypjoy@gmail.com>
2023-07-27 15:38:08 +0900

    feat: 공용 Constants 추가

---
+++ MalBal/MalBal/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MalBal
+//
+//  Created by Joy on 2023/07/27.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    static let glScreenWidth: CGFloat = UIScreen.main.bounds.size.width
+    static let glScreenHeight: CGFloat = UIScreen.main.bounds.size.height
+}
