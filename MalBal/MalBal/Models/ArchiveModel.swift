ebac733f5e9a58722780b3242e009b231f5f1875
JOY <jypjoy@gmail.com>
2023-07-29 03:54:44 +0900

    ✨ feat: 모델 선언

--- MalBal/MalBal/Models/ArchiveModel.swift
+++ MalBal/MalBal/Models/ArchiveModel.swift
@@ -8,19 +8,25 @@
 import Foundation
 import RealmSwift
 
-//class ArchiveModel: Object, ObjectKeyIdentifiable {
-//    @Persisted(primaryKey: true) var id : ObjectId
-//
-//    @Persisted var title = ""
-//    @Persisted var date = ""
-//}
-//
-//
-
 struct ArchiveModel: Identifiable {
     var id = UUID().uuidString
     var title: String
     var date: String
     var offset: CGFloat
     var isSwiped: Bool
+}
+
+class ArchiveRealmModel: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id : ObjectId
+    
+    @Persisted var title: String
+    @Persisted var date: String
+    @Persisted var offset: CGFloat
+    @Persisted var isSwiped: Bool
+}
+
+extension CGFloat: CustomPersistable {
+   public typealias PersistedType = Double
+   public init(persistedValue: Double) { self.init(persistedValue) }
+   public var persistableValue: Double { Double(self) }
 }
