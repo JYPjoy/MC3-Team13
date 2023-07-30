5633f3ae96f35b6bcb74535bd517d8d512c0fca4
JOY <jypjoy@gmail.com>
2023-07-30 17:48:52 +0900

    ✨ feat: UIApplication extension에 tapGestureRecognizer추가

--- MalBal/MalBal/Views/Main/ContentView.swift
+++ MalBal/MalBal/Views/Main/ContentView.swift
@@ -5,6 +5,7 @@ struct ContentView: View {
     var body: some View {
         //mainView()
         ArchiveView(archive: ArchiveRealmModel())
+            .onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
     }
 }
 
