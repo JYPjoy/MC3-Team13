91cfb1a00fb8f7a587700b34091659a12a7518de
JOY <jypjoy@gmail.com>
2023-08-02 15:49:50 +0900

    ✨ feat: Splash 화면 추가

--- MalBal/MalBal/Views/Splash/SplashView.swift
+++ MalBal/MalBal/Views/Splash/SplashView.swift
@@ -14,28 +14,32 @@ struct SplashView: View {
                 Color.main2.ignoresSafeArea()
                 LottieView(jsonName: "SplashLottieFiles", loopMode: .loop).ignoresSafeArea()
                 
-                VStack{
-                    Spacer().frame(height:190)
-                    Text("발표에 적절한").foregroundColor(.white).font(FontManager.shared.appleSDGothicNeo(.extrabold, 24))
-                    Text("스피치 속도 만들기").foregroundColor(.white).font(FontManager.shared.appleSDGothicNeo(.extrabold, 24))
-                    Spacer().frame(height: 50)
-                    Text("말발과 시작해요").foregroundColor(.white).font(FontManager.shared.appleSDGothicNeo(.extrabold, 24))
-                    
-                    NavigationLink {
-                        HomeView()
-                    } label: {
-                        RoundedRectangle(cornerRadius: 16)
-                            .frame(width: GLConstants.glScreenWidth - 48, height:64)
-                            .foregroundColor(Color(UIColor(.main3)))
-                            .overlay(
-                                Text("시작하기")
-                                    .font(FontManager.shared.appleSDGothicNeo(.semibold, 20))
-                                    .foregroundColor(.white)
-                                    .padding()
-                                    .padding(.horizontal, 16)
-                            )
+                ZStack{
+                    VStack{
+                        Spacer().frame(height: 101)
+                        Text("발표에 적절한").foregroundColor(.white).font(FontManager.shared.appleSDGothicNeo(.extrabold, 24))
+                        Text("스피치 속도 만들기").foregroundColor(.white).font(FontManager.shared.appleSDGothicNeo(.extrabold, 24))
+                        Spacer().frame(height: 50)
+                        Text("말발과 시작해요").foregroundColor(.white).font(FontManager.shared.appleSDGothicNeo(.extrabold, 24))
+            
+                        Spacer().frame(height: 520)
+                        
+                        NavigationLink {
+                            HomeView()
+                        } label: {
+                            RoundedRectangle(cornerRadius: 16)
+                                .frame(width: GLConstants.glScreenWidth - 48, height:64)
+                                .foregroundColor(Color(UIColor(.main3)))
+                                .overlay(
+                                    Text("시작하기")
+                                        .font(FontManager.shared.appleSDGothicNeo(.semibold, 20))
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .padding(.horizontal, 16)
+                                )
+                        }
                     }
-                }
+                }.ignoresSafeArea()
             }
         }
     }
