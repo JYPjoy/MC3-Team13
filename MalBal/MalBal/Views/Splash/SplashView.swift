09ff1c97d38c2bed5bef1bbf768900381b07ea55
JOY <jypjoy@gmail.com>
2023-08-02 15:28:42 +0900

    ✨ feat: Splash 애니메이션 추가

--- MalBal/MalBal/Views/Splash/SplashView.swift
+++ MalBal/MalBal/Views/Splash/SplashView.swift
@@ -10,39 +10,30 @@ import SwiftUI
 struct SplashView: View {
     var body: some View {
         NavigationView{
-            
             ZStack {
                 Color.main2.ignoresSafeArea()
+                LottieView(jsonName: "SplashLottieFiles", loopMode: .loop).ignoresSafeArea()
                 
-                ZStack {
-                    Spacer().frame(height: 603)
-                    Ellipse().fill(LinearGradient(colors: [.main1.opacity(0.1), .main1.opacity(0)], startPoint: .top, endPoint: .bottom))
-                        .frame(width: 646, height: 336)
-                }
-                
-                
-                ZStack{
-                    VStack{
-                        Text("발표에 적절한").foregroundColor(.white)
-                        Text("스피치 속도 만들기").foregroundColor(.white)
-                        Spacer().frame(height: 50)
-                        Text("말발과 시작해요").foregroundColor(.white)
-                        
-                        NavigationLink {
-                            HomeView()
-                            
-                        } label: {
-                            RoundedRectangle(cornerRadius: 16)
-                                .frame(width: GLConstants.glScreenWidth - 48, height:64)
-                                .foregroundColor(Color(UIColor(.main3)))
-                                .overlay(
-                                    Text("시작하기")
-                                        .font(FontManager.shared.appleSDGothicNeo(.semibold, 20))
-                                        .foregroundColor(.white)
-                                        .padding()
-                                        .padding(.horizontal, 16)
-                                )
-                        }
+                VStack{
+                    Spacer().frame(height:190)
+                    Text("발표에 적절한").foregroundColor(.white).font(FontManager.shared.appleSDGothicNeo(.extrabold, 24))
+                    Text("스피치 속도 만들기").foregroundColor(.white).font(FontManager.shared.appleSDGothicNeo(.extrabold, 24))
+                    Spacer().frame(height: 50)
+                    Text("말발과 시작해요").foregroundColor(.white).font(FontManager.shared.appleSDGothicNeo(.extrabold, 24))
+                    
+                    NavigationLink {
+                        HomeView()
+                    } label: {
+                        RoundedRectangle(cornerRadius: 16)
+                            .frame(width: GLConstants.glScreenWidth - 48, height:64)
+                            .foregroundColor(Color(UIColor(.main3)))
+                            .overlay(
+                                Text("시작하기")
+                                    .font(FontManager.shared.appleSDGothicNeo(.semibold, 20))
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .padding(.horizontal, 16)
+                            )
                     }
                 }
             }
